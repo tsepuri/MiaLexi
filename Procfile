4a6d9d31f97c72028f86114430b1162665ca2da6
Tarun <starkiller.1413@gmail.com>
2021-05-13 12:44:04 -0400

    Set up for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -jar server/target/mialexi-0.0.1-SNAPSHOT.jar
--- server/src/test/java/com/csds393/mialexi/TextInputControllerTest.java
+++ server/src/test/java/com/csds393/mialexi/TextInputControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import com.csds393.mialexi.controller.TextInputController;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -16,7 +16,8 @@ public class TextInputControllerTest {
 	@Test
 	void testWiki() {
 		TextInputController ti = new TextInputController();
-		assertEquals(ti.getWikipedia("polka"), "The polka is originally a Czech dance and genre of dance music familiar throughout all of Europe and the Americas. It originated in the middle of the nineteenth century in Bohemia, now part of the Czech Republic. The polka remains a popular folk music genre in many European and American countries, and is performed by many folk artists.");
+		assertEquals(ti.getWikipedia("polka").trim(), 
+		"The polka is originally a Czech dance and genre of dance music familiar throughout all of Europe and the Americas. It originated in the middle of the nineteenth century in Bohemia, now part of the Czech Republic. The polka remains a popular folk music genre in many European and American countries, and is performed by many folk artists.");
 	}
 	
 	@Test
@@ -28,8 +29,7 @@ public class TextInputControllerTest {
 	@Test
 	void testPdf() throws IOException {
 		TextInputController ti = new TextInputController();
-		
-		assertEquals(ti.pdfToString(System.getProperty("user.dir")+"\\Test2.pdf").trim(), "Test one two three");
+		assertEquals(ti.pdfToString(System.getProperty("user.dir")+File.separator+"Test2.pdf").trim(), "Test one two three");
 	}
 	
 	@Test
@@ -37,6 +37,6 @@ public class TextInputControllerTest {
 		TextInputController ti = new TextInputController();
 		
 		
-		assertEquals(ti.txtToString(System.getProperty("user.dir")+"\\test1").trim(), "Test one two three");
+		assertEquals(ti.txtToString(System.getProperty("user.dir")+File.separator+"test1").trim(), "Test one two three");
 	}
 }
