0a323d88df9f12e16d6a39f7a43a162ddceb5297
Tarun <starkiller.1413@gmail.com>
2021-05-13 20:35:31 -0400

    Switched table name to Users

--- server/src/main/java/com/csds393/mialexi/model/User.java
+++ server/src/main/java/com/csds393/mialexi/model/User.java
@@ -4,16 +4,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Column;
-
+import javax.persistence.Table;
 
 @Entity
+@Table(name="users")
 public class User {
 
     private Long id;
     private String username;
     private String password;
     private Integer practiceLevel;
-    
+
     @Id
     @GeneratedValue
     public Long getId() {
